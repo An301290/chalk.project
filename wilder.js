97c197cf0a1d28f22e3434c9b1811c302fbc3c06
Angel Sámano Hamud <Angelsam@air-de-angel.localdomain>
2021-03-17 17:13:42 +0100

    Chalk File1

---
+++ wilder.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Okan!'));
+console.log(chalk.red('Luca!'));
+console.log(chalk.white('Johanna!'));
+console.log(chalk.red('Alikki!'));
+console.log(chalk.red('Gordon!'));
